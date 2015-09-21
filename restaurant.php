843290775812a3f2529ff990ddd4153e2303d174
Sorencys <rankie32@hotmail.com>
2015-09-21 12:16:24 +0200

    Restaurant aanpassen

--- restaurant.php
+++ restaurant.php
@@ -1,43 +1,19 @@
 <?php
 
 include 'header.php';
-include 'class/Menu.class.php';
+
 
 ?>
 
-    <div class="container">
-      <ul class="nav nav-tabs">
-        <li class="active"><a data-toggle="tab" href="#home">Voorgerecht</a></li>
-        <li><a data-toggle="tab" href="#menu1">Hoofdgerecht</a></li>
-        <li><a data-toggle="tab" href="#menu2">Nagerecht</a></li>
-        <li><a data-toggle="tab" href="#menu3">Dranken</a></li>
-      </ul>
-
-      <div class="tab-content">
-        <div id="home" class="tab-pane fade in active">
-          <h3>Voorgerecht</h3>
-          <p></p>
-        </div>
-        <div id="menu1" class="tab-pane fade">
-          <h3>Hoofdgerecht</h3>
-          <p></p>
-        </div>
-        <div id="menu2" class="tab-pane fade">
-          <h3>Nagerecht</h3>
-          <p></p>
-        </div>
-        <div id="menu3" class="tab-pane fade">
-          <h3>Dranken</h3>
-          <p></p>
-        </div>
-      </div>
-    </div>
+    <a href="restaurant_menu.php">
+        <img  id="link_menu" alt="W3Schools" src="img/128.jpg" style="width:200px; height:315px;">
+    </a>
+    <a href="restaurant_reserveren.php">
+        <img id="link_reserveren" alt="W3Schools" src="img/reserveren.png">
+    </a>
 
 <?php
-    $menu=new Menu('localhost:3307', 'root', 'usbw');
-        echo $menu -> returnAllMenu(); 
-
-
+  
 include 'footer.php';
 
 ?>
